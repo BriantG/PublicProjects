1f7946239050036e2f5317c928e3516ce98172bd
Briant Guzman <Briant.Guzman@STU.BMCC.CUNY.EDU>
2017-05-17 15:31:00 -0400

    Create HelloWorldDemo.java
    This is a test file

---
+++ HelloWorldDemo.java
@@ -0,0 +1,36 @@
+
+
+
+class HelloWorld {
+
+    public HelloWorld()
+    {
+        System.out.println("Hello! ");
+    }
+
+    public HelloWorld(String name)
+    {
+        System.out.println("Hello " + name);
+    }
+}
+
+public class HelloWorldDemo {
+
+  public static void main(String[] args)
+  {
+  HelloWorld test = null;
+
+  Scanner keyboard = null;
+
+  String line = "";
+
+  
+  keyboard = new Scanner(System.in);
+  
+  line = keyboard.nextLine();
+    
+  test = new HelloWorld(line);
+
+}
+
+}
